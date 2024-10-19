5b2bfb8836a46c9b63a1130452ff9680d932e004
Jiri Pech <jiri.pech@centrum.cz>
2024-10-19 18:57:59 +0200

    fixed widget

--- widgets/current-hour-price-and-index/api.js
+++ widgets/current-hour-price-and-index/api.js
@@ -12,17 +12,18 @@ module.exports = {
         throw new Error('Nenalezeno žádné zařízení');
       }
 
-      const device = devices[0];  // Použijeme první zařízení
+      const device = devices[0]; 
 
       const currentSpotPrice = await device.getCapabilityValue('measure_current_spot_price_CZK');
       const currentSpotIndex = await device.getCapabilityValue('measure_current_spot_index');
 
-      // Získání ceny pro příští hodinu
-      const nextHour = (new Date().getHours() + 1) % 24;
+      // Přidání časové zóny (např. "Europe/Prague")
+      const currentHour = new Date().toLocaleString('en-GB', { hour: 'numeric', hour12: false, timeZone: 'Europe/Prague' });
+      const nextHour = (parseInt(currentHour, 10) + 1) % 24;
+
       const nextHourPrice = await device.getCapabilityValue(`hour_price_CZK_${nextHour}`);
       const nextHourIndex = await device.getCapabilityValue(`hour_price_index_${nextHour}`);
 
-      // Získání průměrné denní ceny
       const averagePrice = await device.getCapabilityValue('daily_average_price');
 
       return {
@@ -38,11 +39,9 @@ module.exports = {
     }
   },
 
-  // Metoda pro registraci callback funkce
   registerUpdateCallback({ homey }, callback) {
     updateCallback = callback;
     
-    // Registrujeme posluchač události
     homey.on('spot_prices_updated', () => {
       if (updateCallback) {
         updateCallback();
